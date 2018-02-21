f71ac6d01bd74d26c7c9cb90f9352f94171c331a
AndrewLeykin <katashi1328@mail.ru>
2018-02-21 12:52:17 +0300

    finish code

--- finish/js/app.js
+++ finish/js/app.js
@@ -2,30 +2,27 @@
 
 (function () {
 
-	var modal = $('.modal'),
-		modalBlock = modal.find('.modal__block'),
-		modalHide = 'modal--hide';
+  var modal = $(".modal"),
+      modalHide = "modal--hide";
 
-	function modalClose(){
-		modal.addClass(modalHide);
-	}
+  function modalClose(){
+    modal.addClass(modalHide);
+  }
 
-	$('.link').click(function() {
-		modal.removeClass(modalHide);
-	});
+  $(".link").click(function() {
+    modal.removeClass(modalHide);
+  });
 
-	$('.modal__close').click(modalClose);
+  $("body").keyup(function(event) {
+    if (event.which == 27 && !modal.hasClass(modalHide)) {
+      modalClose();
+    }
+  });
 
-	$('body').keyup(function(event) {
-		if(event.which == 27 && !(modal.hasClass(modalHide))) {
-			modalClose();
-		}
-	});
-
-	modal.on('click', function (e) {
-		if (!modalBlock.is(e.target) && modalBlock.has(e.target).length === 0) {
-			modalClose();
-		}
-	});
+  modal.on("click", function(e) {
+    if (modal.is(e.target) || $(e.target).hasClass("modal__close")) {
+      modalClose();
+    }
+  });
 
 })();
