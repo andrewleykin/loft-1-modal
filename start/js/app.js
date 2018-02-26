fb518cbc7359eee0afa2d7304e6c26f12d86d497
AndrewLeykin <katashi1328@mail.ru>
2018-02-26 12:57:26 +0300

    for git

--- start/js/app.js
+++ start/js/app.js
@@ -2,6 +2,27 @@
 
 (function () {
 
-	// тут писать код
+	var modal = $('.modal'),
+		modalHide = 'modal--hide';
+
+	$('.link').click(function() {
+		modal.removeClass(modalHide);
+	});
+
+	function closeModal(){
+		modal.addClass(modalHide);
+	}
+
+	$('body').keyup(function(event) {
+		if (event.which == 27 && !modal.hasClass(modalHide)) {
+			modalClose();
+		}
+	});
+
+	modal.click(function(e) {
+		if (modal.is(e.target) || $(e.target).hasClass('modal__close')) {
+			modalClose();
+		}
+	});
 
 })();
