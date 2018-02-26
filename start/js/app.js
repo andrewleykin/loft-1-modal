fe3801dd52029eb520a0b1112705a042525f1cd2
AndrewLeykin <katashi1328@mail.ru>
2018-02-26 12:59:07 +0300

    for start

--- start/js/app.js
+++ start/js/app.js
@@ -2,27 +2,6 @@
 
 (function () {
 
-	var modal = $('.modal'),
-		modalHide = 'modal--hide';
-
-	$('.link').click(function() {
-		modal.removeClass(modalHide);
-	});
-
-	function closeModal(){
-		modal.addClass(modalHide);
-	}
-
-	$('body').keyup(function(event) {
-		if (event.which == 27 && !modal.hasClass(modalHide)) {
-			modalClose();
-		}
-	});
-
-	modal.click(function(e) {
-		if (modal.is(e.target) || $(e.target).hasClass('modal__close')) {
-			modalClose();
-		}
-	});
+	// тут писать код
 
 })();
